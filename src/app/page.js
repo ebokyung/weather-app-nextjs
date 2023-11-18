245835f571d90493a1b36b98a2fb2562f21c2f97
ebokyung <dlqhrud303@naver.com>
2023-11-18 17:04:26 +0900

    feat : index page, not-found page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,19 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <></>
+  return (
+    <>
+      <h1>날씨 앱 만들기 🌈</h1>
+      <p>현재 시간은 ...</p>
+      <button>캐시 비우기</button>
+      <ul>
+        <li>
+          <Link href="/weather/seoul">서울의 날씨는 ...</Link>
+        </li>
+        <li>
+          <Link href="/weather/hongkong">홍콩의 날씨는 ...</Link>
+        </li>
+      </ul>
+    </>
+  )
 }
