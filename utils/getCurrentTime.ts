19e13a8c40cd1b23b260526774512c460eba4e22
ebokyung <dlqhrud303@naver.com>
2023-11-19 14:44:36 +0900

    fix : 현재 시간 fetch url

--- utils/getCurrentTime.ts
+++ utils/getCurrentTime.ts
@@ -1,9 +1,8 @@
 import { CurrentTime } from '../types/currentTime'
 
 export default async function getCurrentTime(): Promise<CurrentTime> {
-  const res = await fetch(
-    'https://timeapi.io/api/Time/current/zone?timeZone=Asia/Seoul',
-  )
+  // 'https://timeapi.io/api/Time/current/zone?timeZone=Asia/Seoul',
+  const res = await fetch(`http://localhost:9999/currentTime`)
 
   if (!res.ok) {
     throw new Error('에러 발생! 시간을 가져오지 못했습니다')
